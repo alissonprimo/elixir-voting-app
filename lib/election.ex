cfc4cc67c97edfd2046cf1d5d90a906a69958895
Alisson Primo <alissonprimo@gmail.com>
2022-07-15 15:25:54 -0300

    views created

--- lib/election.ex
+++ lib/election.ex
@@ -7,4 +7,29 @@ defmodule Election do
     ],
     next_id: 3
   )
+
+  def view(election) do
+    [
+      view_header(),
+      view_body(election),
+      view_footer()
+    ]
+  end
+
+  defp view_header do
+    [
+      "id\tvotes\tname\n",
+      "------------------------------\n"
+    ]
+  end
+
+  defp view_body(election) do
+    Enum.map(election.candidates, fn candidate -> "#{candidate.id}\t#{candidate.votes}\t#{candidate.name}\n" end)
+  end
+
+  defp view_footer do
+    "\ncommands: (n)ame <election>, (a)dd <candidate>, (v)ote <id>, (q)uit\n"
+  end
+
+
 end
